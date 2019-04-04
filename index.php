0a6d2e671a6126dde9b954ef94329d600f9f078c
Anderson <anderson@zumerata.com.br>
2019-04-04 20:07:13 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+$get = isset($_GET['pagina'])? $_GET['pagina']:'';
+
+include 'template/header.php';
+include 'template/navbar.php';
+
+?>
+
+<div class="container">
+
+<?php
+	
+	switch ($get) {
+        case 'listar-produtos':
+            include 'pages/listar-orcamentos.php';
+            break;
+        default:
+            include 'pages/home.php';
+    }
+?>
+
+</div>
+
+<?php include 'template/footer.php' ?>
