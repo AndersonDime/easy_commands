f5dd510fcc7a0572ed0687334ae13be547f939f3
victorgiovanni01 <victor_jacon@estudante.sc.senai.br>
2019-04-25 22:03:51 -0300

    identado telas de sectors

--- pages/alter-sectors.php
+++ pages/alter-sectors.php
@@ -1,55 +1,58 @@
 <?php
-require 'assets/services/session-validate.php';
-$ident =  $_GET["id"];
+    require 'assets/services/session-validate.php';
+    $ident =  $_GET["id"];
 
-include_once("assets/services/products-service.php");
+    include_once("assets/services/products-service.php");
 
-$categ = mysql_getdata("SELECT * FROM setores");
+    $categ = mysql_getdata("SELECT * FROM setores");
 
-$success= isset($_GET["success"]) ? $_GET["success"] : "";
+    $success= isset($_GET["success"]) ? $_GET["success"] : "";
 
-$fail= isset($_GET["fail"]) ? $_GET["fail"] : "";
+    $fail= isset($_GET["fail"]) ? $_GET["fail"] : "";
 
 ?>
 
 
 <form action="?page=edit-sectors" method="post">
-<div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12 col-md-4"></div>
+    <div class="container-fluid">
+        <div class="row">        
             <div class="col-sm-12 col-md-4">
-                <div class="card transparencia">
-                <div class="card-header bg-dark txt-white">Cadastro De Setor</div>
-                    <div class="card-body">
-                    <div class="form-group">
-                        <label>Nome do Setor</label>
-                    <input type="text" class="form-control" id="sec" name="sector" required>
+            <div class="col-sm-12 col-md-4">
+                    <div class="card transparencia">
+                        <div class="card-header bg-dark txt-white">
+                            Cadastro De Setor
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label>Nome do Setor</label>
+                                <input type="text" class="form-control" id="sec" name="sector" required>
+                                <input type="hidden" name="id" value="<?php echo $ident?>">
+                            </div>                                        
+                            <input type="submit" class="btn btn-blue" value="Cadastrar">
 
-                    <input type="hidden" name="id" value="<?php echo $ident?>">
+                        </div>
                     </div>
-
-                        
-                    <input type="submit" class="btn btn-blue" value="Cadastrar">
-
-                </div>
-            </div>
-            <?php
-					//se orçamento foi inserido com sucesso mostra essa mensagem:
+                <?php
+                    //se orçamento foi inserido com sucesso mostra essa mensagem:
                     if ($success):
-                    ?>
-                    <script>
-                        $.notify( "Cadastrado com sucesso", { position:"top center", className: 'success' } );
-                    </script>
-                    <?php endif; ?>
+                ?>
+                <script>
+                    $.notify( "Cadastrado com sucesso", { position:"top center", className: 'success' } );
+                </script>
+                <?php 
+                    endif; 
+                ?>
 
-                     <?php
-					// se houver erro no formulario mostra essa mensagem:
+                <?php
+                    // se houver erro no formulario mostra essa mensagem:
                     if ($fail):
-                    ?>
-                    <script>
-                        $.notify( "Erro ao cadastrar o item", { position:"top center" } );
-                    </script>
-                    <?php endif; ?>
+                ?>
+                <script>
+                    $.notify( "Erro ao cadastrar o item", { position:"top center" } );
+                </script>
+                <?php 
+                    endif; 
+                ?>
             </div>
             <div class="col-sm-12 col-md-4"></div>
         </div>
